43e6f41225d750dba38d452d65bbad06112a4555
yuzuki <nanami2962@gmail.com>
2021-04-18 16:16:30 +0800

    python is awosome

--- ch1/1-1.py
+++ ch1/1-1.py
@@ -18,7 +18,7 @@ class FrenchDeck:
 
 
 suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
-
+ 
 def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suit_values) + suit_values[card.suit]
